132791f2c3348b21164511a9a812075b2f07afcb
Nick Rutten <hello@nickrttn.me>
2017-06-26 23:46:51 +0200

    get started on shopping list

--- routers/menu.js
+++ routers/menu.js
@@ -6,7 +6,10 @@ const ensureLoggedIn = require('../lib/ensureLoggedIn');
 
 const router = new express.Router();
 
-router.use(ensureLoggedIn()).get('/', weeklyMenu);
+router
+  .use(ensureLoggedIn())
+  .get('/', weeklyMenu)
+  .get('/ingredients', getIngredients);
 
 function weeklyMenu(req, res) {
   menu.read(req.user, onmenu);
@@ -16,6 +19,31 @@ function weeklyMenu(req, res) {
       req.flash('error', err.message);
       res.redirect('/preferences');
     }
+
+    res.render('pages/menu', {recipes: menu});
+  }
+}
+
+function getIngredients(req, res) {
+  menu.read(req.user, onmenu);
+
+  function onmenu(err, menu) {
+    if (err) {
+      req.flash('error', err.message);
+      res.redirect('/menu');
+    }
+
+    // let ingredientIds = [];
+
+    menu.forEach(recipe => {
+      debug(recipe);
+      // recipe.getGroup('recipe.ingredients').toArray().forEach(list => {
+      //   debug(list.getLink('component'));
+      //   ingredientIds.push(list.getLink('component').id);
+      // });
+    });
+
+    // debug(ingredientIds);
 
     res.render('pages/menu', {recipes: menu});
   }
